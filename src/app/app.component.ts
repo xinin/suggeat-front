07684b5818d0e665c5c71eba282a1dd89d413cb2
diego.prieto <diego.prieto@beeva.com>
2017-06-17 21:53:19 +0200

    request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {OnInit} from '@angular/core';
 
-import { ConfigService } from 'app/services/Config/config.service';
-import { UtilsService } from 'app/services/Utils/utils.service';
+import {ConfigService} from 'app/services/Config/config.service';
+import {UtilsService} from 'app/services/Utils/utils.service';
 import {ApiService} from "./services/Api/api.service";
 
 @Component({
@@ -22,41 +22,36 @@ export class AppComponent implements OnInit {
   doSomething(): void {
     console.log(this.Config.values);
     console.log(this.Utils.b64Encode(this.Config.values.description));
-    console.log( this.Utils.b64Decode(this.Utils.b64Encode(this.Config.values.description)));
-
+    console.log(this.Utils.b64Decode(this.Utils.b64Encode(this.Config.values.description)));
 
     this.Api.request('/user/prueba').then(
       res => {
-        const data = res.json().data;
-        console.log(data);
+        console.log('GET', res);
       },
       err => {
         console.log(err);
       }
     );
 
-    //this.Api.request('/user/prueba', {test: 'value' }, {method: 'POST'}).then(
-    //  res => {
-    //    const data = res.json().data;
-    //    console.log(data);
-    //  },
-    //  err => {
-    //    console.log(err);
-    //  }
-    //);
-    //
-    //this.Api.request('/user/prueba', {test: 'value' }, {method: 'PUT'}).then(
-    //  res => {
-    //    const data = res.json().data;
-    //    console.log(data);
-    //  },
-    //  err => {
-    //    console.log(err);
-    //  }
-    //);
+    this.Api.request('/user/prueba', {test: 'value'}, {method: this.Api.POST}).then(
+      res => {
+        console.log('POST', res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
 
+    // this.Api.request('/user/prueba', {test: 'value' }, {method: 'PUT'}).then(
+    //  res => {
+    //    const data = res.json().data;
+    //    console.log(data);
+    //  },
+    //  err => {
+    //    console.log(err);
+    //  }
+    // );
 
   }
-
 
 }
--- src/app/services/Api/api.service.ts
+++ src/app/services/Api/api.service.ts
@@ -8,21 +8,66 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class ApiService {
 
+  public GET: any = RequestMethod.Get;
+  public POST: any = RequestMethod.Post;
+  public DELETE: any = RequestMethod.Delete;
+  public PATCH: any = RequestMethod.Patch;
+
   constructor(private http: Http, private config: ConfigService) {
   }
 
   public request(uri: string, data?: any, options?: any): Promise<any> {
-    uri = this.config.values.api.url + uri;
-    if (!options || options.method === 'GET') {
-      return this.get(uri, options || {});
-    } else if (options.method === 'POST') {
-      return this.post(uri, data, options);
-    } else {
-      console.log(options.method + ' no implementado'); // TODO hacer los demas metodos
-    }
+    return new Promise((resolve, reject) => {
+      console.log('Request', uri, data, options);
+
+      uri = this.config.values.api.url + uri;
+      if (!options) {
+        options = {};
+      }
+      if (!options.headers) {
+        options.headers = {};
+      }
+
+      options.headers = {'Content-Type': 'application/json', 'from': 'aaa'};
+      // const headers = new Headers({'Content-Type': 'application/json'});
+      // console.log(headers);
+
+      options = new RequestOptions(options);
+      // options.headers = headers;
+
+      console.log(options);
+
+      if (!options || !options.method || options.method === this.GET) {
+        options.method = this.GET;
+        this.get(uri, options).then(
+          res => {
+            resolve(this.parse(res));
+          }, err => {
+            reject(this.parse(err));
+          }
+        );
+      } else if (options.method === this.POST) {
+        this.post(uri, data, options).then(
+          res => {
+            resolve(this.parse(res));
+          }, err => {
+            reject(this.parse(err));
+          }
+        );
+      } else {
+        console.log(options);
+        console.log(options.method + ' no implementado'); // TODO hacer los demas metodos
+      }
+    });
+  }
+
+  private parse(res: any): any {
+    console.log('RES', res.json().data);
+    return res.json().data;
   }
 
   private get(uri: string, options: any): Promise<any> {
+    console.log(options);
     return this.http.get(uri, options).toPromise()
       .catch(this.handleErrorPromise);
   }
